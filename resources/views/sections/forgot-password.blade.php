2fdf49a8ab8d4090cbd886d8b808da4689407174
Muhammad Qasim <qasimnepster@gmail.com>
2018-05-03 14:31:40 +0500

    Changed popup design

--- resources/views/sections/forgot-password.blade.php
+++ resources/views/sections/forgot-password.blade.php
@@ -1,19 +1,25 @@
-<div class="modal fade bs-forgot-password-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+<div class="modal site-modal fade bs-forgot-password-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
     <div class="modal-dialog modal-forgot-password" role="document">
-        <div class="modal-content" style="padding: 30px; background: #f8f9f9d1;">
-            <button type="button"  class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="font-size: 26px;">&times;</span></button>
-        <form method="POST" action="{{ config('app.url') }}forgot-password">
-            {{ csrf_field() }}
-            <h1 class="text-light text-center">Forgot Password</h1>
-          <div class="form-group" style="margin-top: 20px">
-            <label class="label-lighter" for="F__xemlEmailAddr">Email Address</label>
-            <div class="input-group">
-              <span class="input-group-addon"> <i class="fa fa-user"></i> </span>
-              <input type="email" class="form-control" name="F__xemlEmailAddr" id="F__xemlEmailAddr" placeholder="Enter your Email-Address">
-            </div>
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h2 class="text-light text-center">Forgot Password</h2>
           </div>
-          <button type="submit" class="btn btn-primary" style="width:100%">SEND</button>
-        </form>
+          <div class="modal-body">
+            <form method="POST" action="{{ config('app.url') }}forgot-password">
+                {{ csrf_field() }}
+              <div class="form-group" style="margin-top: 20px">
+                <label class="label-lighter" for="F__xemlEmailAddr">Email Address</label>
+                <div class="input-group">
+                  <span class="input-group-addon"> <i class="fa fa-user"></i> </span>
+                  <input type="email" class="form-control" name="F__xemlEmailAddr" id="F__xemlEmailAddr" placeholder="Enter your Email-Address">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary" >SEND</button>
+          </div>
         </div>
     </div>
 </div>
--- resources/views/sections/login.blade.php
+++ resources/views/sections/login.blade.php
@@ -1,33 +1,39 @@
-<div class="modal fade bs-login-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+<div class="modal site-modal fade bs-login-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
     <div class="modal-dialog modal-login" role="document">
-        <div class="modal-content" style="padding: 30px; background: #f8f9f9d1;">
-            <button type="button"  class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="font-size: 26px;">&times;</span></button>
-        <form method="POST" action="{{ config('app.url') }}login">
-            {{ csrf_field() }}
-            <h1 class="text-light text-center">Login To ttRegistry</h1>
-          <div class="form-group" style="margin-top: 20px">
-            <label class="label-lighter" for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h2 class="text-light text-center">Login to TheRegistryTT</h2>
           </div>
-          <div class="form-group">
-            <label class="label-lighter" for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+          <div class="modal-body">
+            <form method="POST" action="{{ config('app.url') }}login">
+                {{ csrf_field() }}
+              <div class="form-group" style="margin-top: 20px">
+                <label class="label-lighter" for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email">
+              </div>
+              <div class="form-group">
+                <label class="label-lighter" for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="checkbox">
+                <label class="label-lighter">
+                  <input class="minimal" type="checkbox"> Keep me logged in
+                </label>
+              </div>
+              <div class="checkbox">
+                <label class="label-lighter">
+                  <input type="checkbox"> I agree to the website terms
+                </label>
+              </div>
+              <div class="form-group">
+                <a href="javascript:;" class="text-primary modal-fp">Forgot password?</a>
+              </div>
+            </form>
           </div>
-          <div class="checkbox">
-            <label class="label-lighter">
-              <input class="minimal" type="checkbox"> Keep me logged in
-            </label>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary"><i class="fa fa-sign-in" style="color:#fff;padding-right:5px"></i> LOG IN</button>
           </div>
-          <div class="checkbox">
-            <label class="label-lighter">
-              <input type="checkbox"> I agree to the website terms
-            </label>
-          </div>
-          <div class="form-group">
-            <a href="javascript:;" class="text-primary modal-fp">Forgot password?</a>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
         </div>
     </div>
 </div>
